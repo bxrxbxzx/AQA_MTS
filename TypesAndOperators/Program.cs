8865667aae1d1777affed1d9081d11fa53272a62
Aleksandr Kostin <bxrxbxzx@gmail.com>
2023-11-20 00:56:15 +0300

    Finish

--- TypesAndOperators/Program.cs
+++ TypesAndOperators/Program.cs
@@ -1,8 +1,6 @@
-﻿
-// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information
 
 {
-
 /*
     //Это сообщение будет выведено на экран.
     Console.WriteLine("Hello, World!");
@@ -22,27 +20,27 @@
     Console.WriteLine(name);
     name = "Bob2";
     Console.WriteLine(name);
-    
+
     //Литералы
     //логческие
     Console.WriteLine(true);
     Console.WriteLine(false);
-    
+
     //целочисленные
     Console.WriteLine(-10);
     Console.WriteLine(0);
     Console.WriteLine(10);
-    
+
     //вещественные
     Console.WriteLine(1.24);
     Console.WriteLine(1.24F);
-  
+
     //символьные
     //Console.WriteLine('Hello, World!');
     Console.WriteLine('H');
-    
+
     //Console.WriteLine(null);
-    
+
     //управляющая последовательность
     Console.WriteLine("It's a \t nice day!");
     Console.WriteLine("It's a \n nice day!");
@@ -74,7 +72,7 @@
 
     var j1 = null;
     */
-    
+
 /*
 Console.WriteLine("Добро пожаловать в C#");
 Console.Write("Надеюсь Вы");
@@ -97,28 +95,124 @@ Console.WriteLine("Имя: {0} Возраст: {2} Рост: {1}", name, height,
 */
 
 //консольный ввод.
- 
-Console.WriteLine("Введите свое имя: ");
-string? name = Console.ReadLine();
-Console.WriteLine($"Ваше имя:  {name}");
+
+    /*Console.WriteLine("Введите свое имя: ");
+    string? name = Console.ReadLine();
+    Console.WriteLine($"Ваше имя:  {name}");
 
 
-Console.WriteLine("Введите свой возраст: ");
-string? age_str = Console.ReadLine();
-int age = Convert.ToInt32(age_str);
-Console.WriteLine($"Ваш возраст:  {age}");
+    Console.WriteLine("Введите свой возраст: ");
+    string? age_str = Console.ReadLine();
+    int age = Convert.ToInt32(age_str);
+    Console.WriteLine($"Ваш возраст:  {age}");
+
+    byte
+        a = 4;
+    byte result = (byte)(a + 70);*/
 
 
+    /*if (условие)
+    {
+
+    }
+    else
+    {
+
+    }*/
+
+    /*
+    bool a = false;
+    bool b = false;
+    string name1 = "Tom";
+    string name2 = "Tom1";
+    
+    //if ((10 > 5) || b)
+   // if ((name1==name2)||b) такое сравнение некорректно. нужно использовать метод Equals
+   if (name1.Equals(name2)||b)
+    {
+        Console.WriteLine("Верно");
+       
+    }
+    else
+    {
+        Console.WriteLine("Не верно");
+    }
+    */
 
 
-/*
-//Решение нашей задачи.
+    int x = 10;
+    int y = 20;
+    int z; 
+     
+   if ( x < y )
+   {
+       z = x + y; 
+   }
+   else
+   {
+       Console.WriteLine("Не верно");
+   }
+
+   if (x < y)
+   {
+       z = x - y;
+   }
+
+   z = x < y ? x + y : x - y;
+
+
+/*//Решение нашей задачи.
 //Подготовка данных.
-bool isWhiteBreadFresh = true;
-ushort WhiteBreadPrice = 150;
-ushort butterPrice = 230;
-ushort milkPrice = 170;
-float milkFatPercentage = 1.2F;
-*/
+    bool isWhiteBreadFresh = true;
+    ushort WhiteBreadPrice = 150;
+    ushort butterPrice = 230;
+    ushort milkPrice = 170;
+    float milkFatPercentage = 1.2F;
+    ushort iceCreamPrice = 350;
+
+    Console.Write("Введите начальную сумму: "); //ввод сообщения происходит в той же строке, где и текст
+    string? sumString = Console.ReadLine();
+    //метод ReadLine получит стрингу из консоли. Сохранение в переменной sumString.
+    //Знак вопроса ставим потому что readLine может передать null. Если точно значем,
+    //что null те будет, то "?" можно не ставить
+    short sum = Convert.ToInt16(sumString); // сделаем преобразование sum  в int16
+//внесли в программу начальное кол-во денег.
+
+    if (sum > 0) //обработка ситуаций
+    {
+        if (!isWhiteBreadFresh)
+        {
+            Console.WriteLine("Батон не свежий");
+        }
+        else if (sum >= WhiteBreadPrice)
+        {
+            sum = Convert.ToInt16(sum - WhiteBreadPrice);
+            Console.Write($"Купили свежий батон по цене {WhiteBreadPrice}");
+        }
+        else
+        {
+            Console.WriteLine("На батон денег не хватает");
+        }
+
+        sum = (short)(sum - butterPrice);
+
+        if (milkFatPercentage == 1.2F)
+        {
+            sum = Convert.ToInt16(sum - milkPrice);
+        }
+
+        if (sum >= iceCreamPrice)
+        {
+            sum = Convert.ToInt16(sum - iceCreamPrice);
+        }
+
+        Console.WriteLine($"Остаток суммы: {sum}");
+    }
+    else
+    {
+        Console.WriteLine("Сумма не может быть меньше 0");
+    }
+    */
+
 
 }

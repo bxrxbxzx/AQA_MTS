e2f5138ccc2fbd64ccbf1765a46893cf2027f6f2
Aleksandr Kostin <bxrxbxzx@gmail.com>
2023-11-20 02:35:57 +0300

    Added first task- Calculator

--- TypesAndOperators/FirstTask.cs
+++ TypesAndOperators/FirstTask.cs
@@ -4,6 +4,47 @@ public class FirstTask
 {
     public static void Main()
     {
+        //Задание 1. Калькулятор
+
+        Console.Write("Введите первое число: ");
+        int firstNumber = Convert.ToByte(Console.ReadLine());
+
+        Console.Write("Введите допустимую операцию (+, -, *, /) : ");
+        string? operation = Console.ReadLine();
+
+        Console.Write("Введите второе число: ");
+        int secondNumber = Convert.ToByte(Console.ReadLine());
         
+        int result;
+        
+        switch (operation)
+        {
+            case "+":
+                result = firstNumber + secondNumber;  
+                Console.WriteLine($"Ответ : {result}");
+                break;
+            case "-":
+                result = firstNumber - secondNumber;  
+                Console.WriteLine($"Ответ : {result}");
+                break;
+            case "*":
+                result = firstNumber * secondNumber;  
+                Console.WriteLine($"Ответ : {result}");
+                break;
+            case "/":
+               
+                if (secondNumber == 0)
+                {
+                    Console.WriteLine($"На ноль делить нельзя!");
+                }
+                else
+                {
+                    result = firstNumber / secondNumber;
+                    Console.WriteLine($"Ответ : {result}");
+                }
+
+                break;
+        }
+
     }
 }

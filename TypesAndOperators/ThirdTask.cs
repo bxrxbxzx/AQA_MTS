b3bf51ae51a22b753263c51c7c762a75a29134d0
Aleksandr Kostin <bxrxbxzx@gmail.com>
2023-11-25 23:01:16 +0300

    delete comments and non-usable code

--- TypesAndOperators/ThirdTask.cs
+++ TypesAndOperators/ThirdTask.cs
@@ -6,38 +6,10 @@ public class ThirdTask
 {
     public static void Main1()
     {
-        /*Dictionary<string, string> weather = new Dictionary<string, string>();
-
-        var wDictionary = new Dictionary<string, string>()
-        {
-            { "Осень", "Autumn" },
-            { "Зима", "Winter" },
-            { "Весна", "Spring" },
-            { "Лето", "Summer" },
-            { "Дождь", "Rain" },
-            { "Солнце", "Sun" },
-            { "Туман", "Fog" },
-            { "Бриз", "Breeze" },
-            { "Снег", "Snow" },
-            { "Ветер", "Wind" },
-        };
-
-        Console.Write("Введите слово о погоде на русском языке: ");
-
-        if (Console.ReadLine("Осень")
-        {
-            Console.WriteLine(wDictionary<string>);
-        }
-    }*/
-        //не понял, как сделать вывод.
-        
-        
-        //Пошел по простому пути.
-        
         Console.WriteLine("Введите слово о погоде на русском языке: ");
         var weather = Convert.ToString(Console.ReadLine());
-        
-        switch(weather.ToLower())
+
+        switch (weather.ToLower())
         {
             case "осень":
                 Console.WriteLine("autumn");
@@ -73,9 +45,9 @@ public class ThirdTask
                 Console.WriteLine("Слово отсутствует в словаре");
                 break;
         }
-        
     }
 }
+
 
 
     
